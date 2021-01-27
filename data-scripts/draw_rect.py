742c70d186e37ed287676281d65f0a52483e435a
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2021-01-27 16:06:59 +0100

    Add script that draw rectangles around PoI on images

---
+++ data-scripts/draw_rect.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw
+from glob import glob
+
+categories = ["Porifera_ball", "Porifera_vase", "grey", "red", "Porifera_corona"]
+colors = ["lime", "orange", "green", "red", "yellow"]
+
+files = set(glob("data/*.jpg"))
+
+for f in files:
+    source = Image.open(f)
+    draw = ImageDraw.Draw(source)
+
+    img_width, img_height = source.size
+
+    data = open(f.replace(".jpg", ".txt"))
+    for d in data:
+        params = d.split(" ")
+        params = [float(p) for p in params]
+        params[0] = int(params[0])
+
+        color = colors[params[0]]
+        x_center = int(params[1]*img_width)
+        y_center = int(params[2]*img_height)
+        
+        width = int(params[3]*img_width)
+        height = int(params[4]*img_height)
+
+        tl = (x_center - width, y_center - height)
+        br = (x_center + width, y_center + height)
+
+        draw.rectangle((tl, br), fill=None, outline=color, width=10)
+
+    source.save(f.replace("data", "outlined"), "JPEG")
+
+    break
